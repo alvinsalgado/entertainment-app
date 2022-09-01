3ecfaa3c144c841e863e73c1c330be019e707418
Alvin Salgado <alvinsalgado01@gmail.com>
2022-09-01 14:09:13 -0700

    add custom hook

---
+++ src/Hooks/useFetch.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+
+const useFetch = (urlParams) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState({
+    show: false,
+    msg: '',
+  });
+  const [data, setData] = useState(null);
+  const fetchMovies = async (url) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (data.success === false) {
+        setError({ show: true, msg: data.status_message });
+      } else {
+        console.log(data);
+        setData(data.results || data);
+        setError({ show: false, msg: '' });
+      }
+
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies(`${urlParams}`);
+  }, [urlParams]);
+  return { isLoading, error, data };
+};
+
+export default useFetch;
