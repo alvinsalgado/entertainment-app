76ba5334ddbdf363c12f2e52a50085d350083729
Alvin Salgado <alvinsalgado01@gmail.com>
2022-09-06 10:23:48 -0700

    add ContentTrending component

---
+++ src/components/ContentTrending/ContentTrending.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import CardTrending from '../CardTrending/CardTrending';
+
+import './ContentTrending.scss';
+
+const ContentTrending = ({ data, isLoading, limit, heading }) => {
+  if (isLoading) {
+    return <div className='loading'></div>;
+  }
+
+  return (
+    <div className='trending-content-container'>
+      <h1 className='fs-900'>{heading}</h1>
+      <div className='media-scroller snaps-inline'>
+        {data.slice(0, limit).map((movie) => {
+          return <CardTrending movie={movie} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ContentTrending;
