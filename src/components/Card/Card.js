ba106034e9272d92597888ce929c95ce52230abc
Alvin Salgado <alvinsalgado01@gmail.com>
2022-09-12 21:49:57 -0700

    add Card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Bookmark from '../Bookmark/Bookmark';
+import { images } from '../../utils/images';
+import { Link } from 'react-router-dom';
+import './Card.scss';
+
+const IMG_API = 'https://image.tmdb.org/t/p/original/';
+
+const Card = ({
+  title,
+  media_type,
+  name,
+  backdrop_path,
+  movie,
+  release_date,
+  first_air_date,
+  id,
+  media,
+}) => {
+  if (media_type) {
+    media = media_type;
+  }
+
+  return (
+    <Link to={`/selected/${id}`} state={{ from: media }}>
+      <div className='card-container'>
+        <img className='card-img' src={IMG_API + backdrop_path} alt='poster' />
+        <Bookmark movie={movie} media={media} />
+
+        <ul class='fs-400 card-body'>
+          {release_date && <li>{release_date.slice(0, 4)}</li>}
+          {first_air_date && <li>{first_air_date.slice(0, 4)}</li>}
+
+          <li>
+            <img
+              className='media-icon'
+              src={
+                media_type === 'movie'
+                  ? images.categoryMovie
+                  : images.categoryTv
+              }
+              alt='icon'
+            />
+            {media === 'movie' ? 'Movie' : 'TV Series'}
+          </li>
+        </ul>
+
+        <div class='fs-600'>{title || name}</div>
+      </div>
+    </Link>
+  );
+};
+
+export default Card;
