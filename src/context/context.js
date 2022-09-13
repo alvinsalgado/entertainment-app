9e42f203cfe4aa80f52d3eec018038c2135c9460
Alvin Salgado <alvinsalgado01@gmail.com>
2022-09-13 14:02:36 -0700

    add funtion to add movie and local storage

--- src/context/context.js
+++ src/context/context.js
@@ -2,9 +2,10 @@ import React, { useState, useContext, useEffect } from 'react';
 import useFetch from '../Hooks/useFetch';
 
 const TRENDING_API_ENDPOINT = `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_MOVIE_API_KEY}`;
+const POPULAR_MOVIE_ENDPOINT = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&language=en-US&page=1`;
+const POPULAR_TV_ENDPOINT = `https://api.themoviedb.org/3/tv/popular?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&language=en-US&page=1`;
 
 const AppContext = React.createContext();
-
 const AppProvider = ({ children }) => {
   const [query, setQuery] = useState('');
   const [bookmarked, setBookmarked] = useState([]);
@@ -15,22 +16,61 @@ const AppProvider = ({ children }) => {
     data: trendingMovies,
   } = useFetch(`${TRENDING_API_ENDPOINT}`);
 
+  const { data: popularMovies } = useFetch(`${POPULAR_MOVIE_ENDPOINT}`);
+
+  const { data: popularTV } = useFetch(`${POPULAR_TV_ENDPOINT}`);
+
+  useEffect(() => {
+    const moviesBookmarked = JSON.parse(localStorage.getItem('data'));
+
+    if (moviesBookmarked) {
+      setBookmarked(moviesBookmarked);
+    }
+  }, []);
+
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem('data', JSON.stringify(items));
+  };
+
+  const addMovie = (movie, media, toggle) => {
+    if (toggle) {
+      const newBookmarkedList = bookmarked.filter(
+        (bookmarked) => bookmarked.id !== movie.id
+      );
+
+      setBookmarked(newBookmarkedList);
+      saveToLocalStorage(newBookmarkedList);
+    } else {
+      const newMovie = {
+        ...movie,
+        media,
+      };
+      const newBookmarkedList2 = [...bookmarked, newMovie];
+      setBookmarked(newBookmarkedList2);
+      saveToLocalStorage(newBookmarkedList2);
+    }
+  };
+
   return (
     <AppContext.Provider
       value={{
         query,
         setQuery,
+        addMovie,
         bookmarked,
         setBookmarked,
         trendingLoading,
+        trendingError,
         trendingMovies,
+        popularMovies,
+        popularTV,
       }}
     >
       {children}
     </AppContext.Provider>
   );
 };
-// make sure use
+
 export const useGlobalContext = () => {
   return useContext(AppContext);
 };
