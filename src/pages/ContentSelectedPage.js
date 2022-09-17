8b2cf8dd90b4e9bb7f96d9f1c1ced4289e97dc0c
Alvin Salgado <alvinsalgado01@gmail.com>
2022-09-16 21:01:37 -0700

    add content selected page

---
+++ src/pages/ContentSelectedPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContentSelected from '../components/ContentSelected/ContentSelected';
+
+const ContentSelectedPage = () => {
+  return (
+    <div className=''>
+      <ContentSelected />
+    </div>
+  );
+};
+
+export default ContentSelectedPage;
